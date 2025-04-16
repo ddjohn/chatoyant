296aeebd1786bf1aa041d9cc31a8dc7d06eb5f2d
David Johansson <David Johansson>
2025-04-16 23:15:56 +0200

    spotless

--- chatoyant/src/main/java/com/avelon/chatoyant/DajoNavigationView.kt
+++ chatoyant/src/main/java/com/avelon/chatoyant/DajoNavigationView.kt
@@ -4,17 +4,19 @@ import android.content.Context
 import android.util.AttributeSet
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class DajoNavigationView: BottomNavigationView {
+class DajoNavigationView : BottomNavigationView {
+    constructor(context: Context) : super(context)
 
-    constructor(context: Context): super(context)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
 
-    constructor(context: Context, attrs: AttributeSet): super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int): super(context, attrs, defStyleAttr)
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int) : super(
+        context,
+        attrs,
+        defStyleAttr,
+        defStyleRes,
+    )
 
-    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int): super(context, attrs, defStyleAttr, defStyleRes)
-
-    override fun getMaxItemCount(): Int {
-        return 10
-    }
+    override fun getMaxItemCount(): Int = 10
 }
