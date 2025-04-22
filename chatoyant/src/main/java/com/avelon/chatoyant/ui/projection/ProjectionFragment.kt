0576fa686e928a550a6e2306760c7969865bcfe9
David Johansson <ddjohn@gmail.com>
2025-04-22 22:24:40 +0200

    fixing_ci_again

--- chatoyant/src/main/java/com/avelon/chatoyant/ui/projection/ProjectionFragment.kt
+++ chatoyant/src/main/java/com/avelon/chatoyant/ui/projection/ProjectionFragment.kt
@@ -8,26 +8,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.widthIn
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import com.avelon.chatoyant.databinding.FragmentProjectionBinding
 import com.avelon.chatoyant.logging.DLog
-import com.google.accompanist.permissions.ExperimentalPermissionsApi
-import com.google.accompanist.permissions.isGranted
-import com.google.accompanist.permissions.rememberPermissionState
-import com.google.accompanist.permissions.shouldShowRationale
 
 class ProjectionFragment : Fragment() {
     companion object {
@@ -61,7 +44,6 @@ class ProjectionFragment : Fragment() {
 
         return root
     }
-
 
     override fun onDestroyView() {
         super.onDestroyView()
